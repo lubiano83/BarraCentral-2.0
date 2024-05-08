59496ce426157716c9b156e2dbe88416d444e6b8
lubiano83 <lubiano83@gmail.com>
2024-05-08 16:58:01 -0400

    modificando vista del mapa

--- src/components/MapPreview.jsx
+++ src/components/MapPreview.jsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, View } from "react-native";
 import React from "react";
 import { googleMapsApiKey } from "../databases/googleMaps";
 
@@ -7,19 +7,8 @@ const MapPreview = ({ location }) => {
     const mapPreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=13&size=300x300&maptype=roadmap&markers=color:red%7Clabel:Me%7C${location.latitude},${location.longitude}&key=${googleMapsApiKey}`
 
     return (
-        <View style={styles.mapPreview}>
-            <Image style={styles.mapImage} source={{ uri: mapPreviewUrl }} />
+        <View style={{justifyContent: "center", alignItems: "center", width: "100%"}}>
+            <Image style={{width: "100%", aspectRatio: 1/1}} source={{ uri: mapPreviewUrl }} />
         </View>
     );
 }; export default MapPreview;
-
-const styles = StyleSheet.create({
-    mapPreview: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    mapImage: {
-        width: 300,
-        height: 300,
-    },
-});
--- src/screens/LocationSelector.jsx
+++ src/screens/LocationSelector.jsx
@@ -19,24 +19,24 @@ const LocationSelector = ({ navigation }) => {
   return (
     <>
         <Header title="My Address"/>
-        <View style={{paddingTop: 20, flex: 1, alignItems: "center", justifyContent: "flex-start", paddingHorizontal: 20, gap: 20, backgroundColor: blackColor}}>
-        {/* Flatlist con las directions */}
-        {location ? (
-            <>
-            <Text style={{fontSize: 18, color: whiteColor}}>
-                Lat: {location.latitude}, long: {location.longitude}.
-            </Text>
-            <MapPreview location={location} />
-            <Text style={{fontSize: 20, color: whiteColor}}>Address: {address}</Text>
-            <AddButton onPress={confirmAndBack} title="Confirm address" />
-            </>
-        ) : (
-            <>
-            <View style={{width: 200, height: 200, borderWidth: 2, borderColor: blackColor, padding: 10, justifyContent: "center", alignItems: "center"}}>
-                <Text>{error}</Text>
-            </View>
-            </>
-        )}
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "flex-start", gap: 20, backgroundColor: blackColor}}>
+            {/* Flatlist con las directions */}
+            {location ? (
+                <>
+                    <MapPreview location={location} />
+                    <View style={{paddingHorizontal: 20, width: "100%", gap: 20, alignItems: "center"}}>
+                        <Text style={{fontSize: 18, color: whiteColor}}>Lat: {location.latitude}, long: {location.longitude}.</Text>
+                        <Text style={{fontSize: 20, color: whiteColor}}>Address: {address}</Text>
+                        <AddButton onPress={confirmAndBack} title="Confirm address" />
+                    </View>
+                </>
+            ) : (
+                <>
+                    <View style={{width: 200, height: 200, borderWidth: 2, borderColor: blackColor, padding: 10, justifyContent: "center", alignItems: "center"}}>
+                        <Text>{error}</Text>
+                    </View>
+                </>
+            )}
         </View>
     </>
   );
